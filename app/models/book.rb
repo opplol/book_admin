0e9afa497b74241c3a9fc087eaaacc9be3673210
lee sh <colra03@gmail.com>
2017-11-19 18:24:38 +0900

    create book table and add scope

---
+++ app/models/book.rb
@@ -0,0 +1,5 @@
+class Book < ActiveRecord::Base
+  default_scope -> { order("published_on desc") }
+  scope :costly, -> { where("price > ?", 3000) }
+  scope :written_about, ->(theme) { where("name like ?", "%#{theme}%") }
+end
